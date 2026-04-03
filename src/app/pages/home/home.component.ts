03c85b15480e98b8ae32bef74a00a6916500437f
Maxime Nardelli <maxime.nardelli@gmail.com>
2026-04-03 18:45:23 +0200

    feat(core): ajout d’un cache pour ne charger les données olympiques qu’une seule fois
    Le service OlympicDataService mémorise désormais les données après le premier chargement, évitant ainsi les requêtes multiples et optimisant les accès pour toute l’application.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,10 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit, inject, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  inject,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
@@ -8,23 +13,28 @@ import { Country } from '../../models/country.model';
 import { Participation } from '../../models/participation.model';
 
 import { OlympicDataService } from '../../services/olympic-data.service';
-import { ErrorMessageComponent } from '../shared/error-message.component';
-import { LoadingIndicatorComponent } from '../shared/loading-indicator.component';
+import { ErrorMessageComponent } from '../../shared/error-message.component';
+import { LoadingIndicatorComponent } from '../../shared/loading-indicator.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule, ErrorMessageComponent, LoadingIndicatorComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ErrorMessageComponent,
+    LoadingIndicatorComponent,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
-  public pieChart!: Chart<"pie", number[], string>;
+  public pieChart!: Chart<'pie', number[], string>;
   public totalCountries = 0;
   public totalJOs = 0;
-  public error!:string
-  titlePage = "Medals per Country";
+  public error!: string;
+  titlePage = 'Medals per Country';
   public loading = true;
 
   private http = inject(HttpClient);
@@ -36,11 +46,23 @@ export class HomeComponent implements OnInit {
     this.olympicService.getOlympicCountries().subscribe(
       (data: Country[]) => {
         if (data && data.length > 0) {
-          this.totalJOs = Array.from(new Set(data.map((i: Country) => i.participations.map((f: Participation) => f.year)).flat())).length;
+          this.totalJOs = Array.from(
+            new Set(
+              data
+                .map((i: Country) =>
+                  i.participations.map((f: Participation) => f.year),
+                )
+                .flat(),
+            ),
+          ).length;
           const countries: string[] = data.map((i: Country) => i.country);
           this.totalCountries = countries.length;
-          const medals = data.map((i: Country) => i.participations.map((i: Participation) => i.medalsCount));
-          const sumOfAllMedalsYears = medals.map((i: number[]) => i.reduce((acc: number, val: number) => acc + val, 0));
+          const medals = data.map((i: Country) =>
+            i.participations.map((i: Participation) => i.medalsCount),
+          );
+          const sumOfAllMedalsYears = medals.map((i: number[]) =>
+            i.reduce((acc: number, val: number) => acc + val, 0),
+          );
           this.buildPieChart(countries, sumOfAllMedalsYears);
         }
         this.loading = false;
@@ -48,35 +70,51 @@ export class HomeComponent implements OnInit {
       (error: HttpErrorResponse) => {
         this.error = error.message;
         this.loading = false;
-      }
+      },
     );
   }
 
   buildPieChart(countries: string[], sumOfAllMedalsYears: number[]) {
-    const pieChart = new Chart("DashboardPieChart", {
+    const pieChart = new Chart('DashboardPieChart', {
       type: 'pie',
       data: {
         labels: countries,
-        datasets: [{
-          label: 'Medals',
-          data: sumOfAllMedalsYears,
-          backgroundColor: ['#0b868f', '#adc3de', '#7a3c53', '#8f6263', 'orange', '#94819d'],
-          hoverOffset: 4
-        }],
+        datasets: [
+          {
+            label: 'Medals',
+            data: sumOfAllMedalsYears,
+            backgroundColor: [
+              '#0b868f',
+              '#adc3de',
+              '#7a3c53',
+              '#8f6263',
+              'orange',
+              '#94819d',
+            ],
+            hoverOffset: 4,
+          },
+        ],
       },
       options: {
         aspectRatio: 2.5,
         onClick: (e) => {
           if (e.native) {
-            const points = pieChart.getElementsAtEventForMode(e.native, 'point', { intersect: true }, true)
+            const points = pieChart.getElementsAtEventForMode(
+              e.native,
+              'point',
+              { intersect: true },
+              true,
+            );
             if (points.length) {
               const firstPoint = points[0];
-              const countryName = pieChart.data.labels ? pieChart.data.labels[firstPoint.index] : '';
+              const countryName = pieChart.data.labels
+                ? pieChart.data.labels[firstPoint.index]
+                : '';
               this.router.navigate(['country', countryName]);
             }
           }
-        }
-      }
+        },
+      },
     });
     this.pieChart = pieChart;
   }
